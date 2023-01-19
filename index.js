6766ef092d434d1687bb4ef92dd0b511ebb72afc
liarley.personal@gmail.com <liarley.personal@gmail.com>
2023-01-19 13:12:37 -0300

    Reveal elements on scroll created

---
+++ index.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll(".img")
+
+window.onload = scrolling // call at least once time
+window.onscroll = scrolling
+
+function scrolling() {
+  const viewportHeight = window.innerHeight - window.innerHeight / 3
+  images.forEach(img => {
+    const imgTop = img.getBoundingClientRect().top
+    if (imgTop < viewportHeight)
+      img.style.animationPlayState = 'running'
+  })
+}
